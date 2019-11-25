bfe9860ffbc60d511b9219cc1c3f96483fce90b5
Alex <oleksandr.hryhorchuk@gmail.com>
2019-11-25 22:48:54 +0200

    Setup project

---
+++ Respire/Common/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Respire
+//
+//  Created by Alex on 11/25/19.
+//  Copyright © 2019 aleksandr.grigorchuk. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
